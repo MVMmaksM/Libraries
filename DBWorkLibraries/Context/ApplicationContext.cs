fd85ab7af5d91473c93e3f6d7ee3cb502f38d9e5
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-28 16:53:40 +0500

    Create dbSet Details

---
+++ DBWorkLibraries/Context/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using DBWorkLibraries.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBWorkLibraries.Context
+{
+    internal class ApplicationContext : DbContext
+    {
+        private string _connectionString;
+        
+        public DbSet<Detail> Details { get; set; }
+
+        public ApplicationContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
