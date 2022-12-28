831311a434767a2741b82a2e877bad79cfcc4508
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-28 16:41:48 +0500

    Create class AppContext

---
+++ DBWorkLibraries/Context/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBWorkLibraries.Context
+{
+    internal class AppContext:DbContext
+    {
+        private string _connectionString;
+
+        public AppContext(string connectionString)
+        {
+            _connectionString= connectionString;
+        }
+
+
+    }
+}
