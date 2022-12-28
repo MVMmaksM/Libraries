90e3380648ac921979386192086d237250b65ff5
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-28 16:15:24 +0500

    Create class Sheet

--- DBWorkLibraries/Model/Sheet.cs
+++ DBWorkLibraries/Model/Sheet.cs
@@ -2,16 +2,19 @@
 
 namespace DBWorkLibraries.Model
 {
-    internal class Material
+    internal class Sheet
     {
         public long Id { get; set; }
 
         [MaxLength(100)]
         public string Title { get; set; }
-        
+
         [MaxLength(100)]
         public string FullName { get; set; }
 
-        public float Thick { get; set; }
+        public float Width { get; set; }
+
+        public float Height { get; set; }
+
     }
 }
