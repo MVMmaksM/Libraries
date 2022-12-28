134cc12c8e61ab05bea3b6078d0332fcca0e30bf
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-28 16:06:20 +0500

    Create class Detail

---
+++ DBWorkLibraries/Model/Detail.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBWorkLibraries.Model
+{
+    internal class Detail
+    {
+        public long Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        public byte[] Contours { get; set; }
+    }
+}
