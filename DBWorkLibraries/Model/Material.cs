3d73815b7b0789b1a385adc5d7a1b5fc426d1fc9
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-28 16:10:24 +0500

    Create class Material

---
+++ DBWorkLibraries/Model/Material.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBWorkLibraries.Model
+{
+    internal class Material
+    {
+        public long Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+        
+        [MaxLength(100)]
+        public string FullName { get; set; }
+        public float Thick { get; set; }
+    }
+}
