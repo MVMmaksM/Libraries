f977bf6e6d7d156feaaa677e3bf3222fe5c50dbb
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-29 16:17:07 +0500

    Add DbSet

--- DBWorkTest/Context/ApplicationContext.cs
+++ DBWorkTest/Context/ApplicationContext.cs
@@ -5,13 +5,18 @@ namespace DBWorkLibraries.Context
 {
     internal class ApplicationContext : DbContext
     {
-        private string _connectionString;
-        
+        private string _connectionString;        
         public DbSet<Detail> Details { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Sheet> Sheets { get; set; }
+        public DbSet<CuttingMap> CuttingMaps { get; set; }
+        public DbSet<CuttingMapDetail> CuttingMapDetails { get; set; }
+
 
         public ApplicationContext(string connectionString)
         {
             _connectionString = connectionString;
+            Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- DBWorkTest/Program.cs
+++ DBWorkTest/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace DBWorkTest
+﻿using DBWorkLibraries.Context;
+
+namespace DBWorkTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionString = "Server=P45-DB08; Database = Test; Trusted_Connection = True; Encrypt = False ";
+            
+            ApplicationContext appContext = new ApplicationContext(connectionString);        
         }
     }
 }
