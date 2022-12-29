e8a434e484b07d223f3d30d0bb65e6e83c99b169
MVMmaksM <vaganov.maksim94@yandex.ru>
2022-12-29 15:40:44 +0500

    Create testWork

---
+++ DBWorkTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBWorkTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
