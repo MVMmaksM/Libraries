d5602e5ad86b14a3d1966741647d4983b43dde53
MVMmaksM <vaganov.maksim@mail.ru>
2023-01-01 11:46:42 +0500

    Set name PK Material

--- DBWorkTest/Entities/Configuring/MaterialConfig.cs
+++ DBWorkTest/Entities/Configuring/MaterialConfig.cs
@@ -8,6 +8,7 @@ namespace DBWorkTest.Entities.Configuring
     {
         public void Configure(EntityTypeBuilder<Material> entity)
         {
+            entity.HasKey(m => m.Id).HasName("PK_Material");
             entity.Property(p=>p.Title).HasMaxLength(100).IsRequired(true);
             entity.Property(p=>p.FullName).HasMaxLength(100).IsRequired(true);
             entity.Property(p=>p.Thick).IsRequired(true);
