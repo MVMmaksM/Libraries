dcf188d545288ec214ed75baedb09922b34c52a1
MVMmaksM <vaganov.maksim@mail.ru>
2023-01-01 11:45:33 +0500

    Set name PK CuttingMap and Detail

--- DBWorkTest/Entities/Configuring/CuttingMapConfig.cs
+++ DBWorkTest/Entities/Configuring/CuttingMapConfig.cs
@@ -4,14 +4,13 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DBWorkTest.Entities.Configuring
 {
-    internal class DetailConfig : IEntityTypeConfiguration<Detail>
+    internal class CuttingMapConfig : IEntityTypeConfiguration<CuttingMap>
     {
-        public void Configure(EntityTypeBuilder<Detail> entity)
+        public void Configure(EntityTypeBuilder<CuttingMap> entity)
         {
-            entity.HasKey(d => d.Id).HasName("PK_Detail");
+            entity.HasKey(c => c.Id).HasName("PK_CuttingMap");
             entity.Property(p => p.Title).HasMaxLength(100).IsRequired(true);
             entity.Property(p => p.FullName).HasMaxLength(100).IsRequired(true);
-            entity.Property(p => p.Contours).IsRequired(true);
         }
     }
 }
