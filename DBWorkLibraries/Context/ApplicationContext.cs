b8c2e14071af46231b3118740fe046184658a74c
MVMmaksM <vaganov.maksim@mail.ru>
2023-01-03 21:35:24 +0500

    Create context and DBWork Libraries

--- DBWorkLibraries/Context/ApplicationContext.cs
+++ DBWorkLibraries/Context/ApplicationContext.cs
@@ -1,22 +1,32 @@
 ﻿using DBWorkLibraries.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
 
 namespace DBWorkLibraries.Context
 {
     internal class ApplicationContext : DbContext
     {
         private string _connectionString;
-        
-        public DbSet<Detail> Details { get; set; }
-
+        public DbSet<Detail> Detail { get; set; } = null!;        
+        public DbSet<Material> Material { get; set; }
+        public DbSet<Sheet> Sheet { get; set; }
+        public DbSet<CuttingMap> CuttingMap { get; set; }
+        public DbSet<CuttingMapDetail> CuttingMapDetail { get; set; }
+               
         public ApplicationContext(string connectionString)
         {
-            _connectionString = connectionString;
+            _connectionString = connectionString;          
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+        protected override void OnModelCreating(ModelBuilder entityBuilder)
+        {
+            entityBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());           
         }
     }
 }
---
+++ DBWorkLibraries/DBWork/DataBaseWork.cs
@@ -0,0 +1,149 @@
+﻿using DBWorkLibraries.Context;
+using DBWorkLibraries.Entities;
+using DBWorkTest.Entities.Configuring;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBWorkLibraries.DBWork
+{
+    internal class DataBaseWork
+    {
+        public string ConnectionString { get; }
+        public DataBaseWork(string connectionString)
+        {
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                ConnectionString = connectionString;
+            }
+            else
+            {
+                throw new Exception("ConnectionString IsNullOrWhiteSpace");
+            }
+        }
+        public bool DataBaseCreated()
+        {
+            using (var appContext = new ApplicationContext(ConnectionString))
+            {
+                return appContext.Database.EnsureCreated();
+            }
+        }
+
+        public int InsertDetail(ICollection<Detail> details)
+        {
+            if (details is not null)
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    appContext.Detail.AddRange(details);
+                    return appContext.SaveChanges();
+                }
+            }
+            else
+            {
+                throw new Exception("Details is null");
+            }
+        }
+
+        public int InsertMaterial(ICollection<Material> materials)
+        {
+            if (materials is not null)
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    appContext.Material.AddRange(materials);
+                    return appContext.SaveChanges();
+                }
+            }
+            else
+            {
+                throw new Exception("Materials is null");
+            }
+        }
+
+        public int InsertSheet(ICollection<Sheet> sheets)
+        {
+            if (sheets is not null)
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    appContext.Sheet.AddRange(sheets);
+                    return appContext.SaveChanges();
+                }
+            }
+            else
+            {
+                throw new Exception("Sheets is null");
+            }
+        }
+
+        public int InsertCuttingMap(ICollection<CuttingMap> cuttingMaps)
+        {
+            if (cuttingMaps is not null)
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    appContext.CuttingMap.AddRange(cuttingMaps);
+                    return appContext.SaveChanges();
+                }
+            }
+            else
+            {
+                throw new Exception("CuttingMaps is null");
+            }
+        }
+
+        public ICollection<Detail> SelectSqlFromDetail(string sqlQuery)
+        {
+            if (!string.IsNullOrWhiteSpace(sqlQuery))
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    return appContext.Detail.FromSqlRaw(sqlQuery).ToList();
+                }
+            }
+            else
+            {
+                throw new Exception("SqlQuery is null");
+            }
+        }
+
+        public ICollection<Material> SelectSqlFromMaterial(string sqlQuery)
+        {
+            if (!string.IsNullOrWhiteSpace(sqlQuery))
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    return appContext.Material.FromSqlRaw(sqlQuery).ToList();
+                }
+            }
+            else
+            {
+                throw new Exception("SqlQuery is null");
+            }
+        }
+
+        public ICollection<Sheet> SelectSqlFromSheet(string sqlQuery)
+        {
+            if (!string.IsNullOrWhiteSpace(sqlQuery))
+            {
+                using (var appContext = new ApplicationContext(ConnectionString))
+                {
+                    return appContext.Sheet.FromSqlRaw(sqlQuery).ToList();
+                }
+            }
+            else
+            {
+                throw new Exception("SqlQuery is null");
+            }
+        }
+
+        //public ICollection<Detail> SearchDetail(Func<Detail, bool> predicate)
+        //{
+        //    using (var appContext = new ApplicationContext(ConnectionString))
+        //    {
+        //        IQueryable<Detail> a = appContext.Detail;
+        //        var c = a.Where(predicate).ToList();                
+        //        return c;
+        //    }
+        //}
+    }
+}
